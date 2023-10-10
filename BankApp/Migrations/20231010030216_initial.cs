8b3c1abd57902c97cc37f1625381eb02e34aabe0
James Maye <jamesmaye6094@gmail.com>
2023-10-09 23:18:46 -0400

    add default connection for database connection

---
+++ BankApp/Migrations/20231010030216_initial.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountStatusType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountStatusDescription = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__AccountS__3214EC07DE8B303B", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountTypeDescription = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__AccountT__3214EC072C3DCD1D", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionTypeName = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    TransactionTypeDescription = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Transact__3214EC0749943290", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLogin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserLogin = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Password = table.Column<byte[]>(type: "binary(64)", fixedLength: true, maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__UserLogi__3214EC0790E21248", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrentBalance = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    AvailableBalance = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    AccountNumber = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    MadeOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AccountTypeId = table.Column<int>(type: "int", nullable: true),
+                    AccountStatusTypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Account__3214EC07BFDBBE79", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Account__Account__403A8C7D",
+                        column: x => x.AccountTypeId,
+                        principalTable: "AccountType",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Account__Account__412EB0B6",
+                        column: x => x.AccountStatusTypeId,
+                        principalTable: "AccountStatusType",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Middle_Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Last_Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Suffix = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Address1 = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Address2 = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    City = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    State = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    ZipCode = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    Email = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    HomePhone = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    MobilePhone = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    SSN = table.Column<string>(type: "varchar(8)", unicode: false, maxLength: 8, nullable: true),
+                    Profile_Img = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: true),
+                    UserLoginId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__3214EC079FDB6FAF", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Customer__UserLo__398D8EEE",
+                        column: x => x.UserLoginId,
+                        principalTable: "UserLogin",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoginAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserLoginId = table.Column<int>(type: "int", nullable: true),
+                    AccountId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__LoginAcc__3214EC07B74D6085", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__LoginAcco__Accou__5070F446",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__LoginAcco__UserL__4F7CD00D",
+                        column: x => x.UserLoginId,
+                        principalTable: "UserLogin",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAccount",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountId = table.Column<int>(type: "int", nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__3214EC07D83A739D", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__CustomerA__Accou__440B1D61",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__CustomerA__Custo__44FF419A",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionAmount = table.Column<decimal>(type: "decimal(10,2)", nullable: true),
+                    NewBalance = table.Column<decimal>(type: "decimal(10,2)", nullable: true),
+                    TransactionTypeId = table.Column<int>(type: "int", nullable: true),
+                    AccountId = table.Column<int>(type: "int", nullable: true),
+                    CustomerId = table.Column<int>(type: "int", nullable: true),
+                    UserLoginId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Transact__3214EC0765DF341A", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK__Transacti__Accou__4AB81AF0",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Transacti__Custo__4BAC3F29",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Transacti__Trans__49C3F6B7",
+                        column: x => x.TransactionTypeId,
+                        principalTable: "TransactionType",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK__Transacti__UserL__4CA06362",
+                        column: x => x.UserLoginId,
+                        principalTable: "UserLogin",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_AccountStatusTypeId",
+                table: "Account",
+                column: "AccountStatusTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_AccountTypeId",
+                table: "Account",
+                column: "AccountTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customer_UserLoginId",
+                table: "Customer",
+                column: "UserLoginId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccount_AccountId",
+                table: "CustomerAccount",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAccount_CustomerId",
+                table: "CustomerAccount",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoginAccount_AccountId",
+                table: "LoginAccount",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoginAccount_UserLoginId",
+                table: "LoginAccount",
+                column: "UserLoginId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionHistory_AccountId",
+                table: "TransactionHistory",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionHistory_CustomerId",
+                table: "TransactionHistory",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionHistory_TransactionTypeId",
+                table: "TransactionHistory",
+                column: "TransactionTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionHistory_UserLoginId",
+                table: "TransactionHistory",
+                column: "UserLoginId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerAccount");
+
+            migrationBuilder.DropTable(
+                name: "LoginAccount");
+
+            migrationBuilder.DropTable(
+                name: "TransactionHistory");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "TransactionType");
+
+            migrationBuilder.DropTable(
+                name: "AccountType");
+
+            migrationBuilder.DropTable(
+                name: "AccountStatusType");
+
+            migrationBuilder.DropTable(
+                name: "UserLogin");
+        }
+    }
+}
